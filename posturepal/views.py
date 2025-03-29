05699383d7f3f7f21656bcf31349cb8c4b511632
Amelia Chen <amchen.0t7@gmail.com>
2025-03-29 18:37:11 -0400

    navigation

--- posturepal/views.py
+++ posturepal/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'posturepal/index.html')
+
+def live_checker(request):
+    return render(request, 'posturepal/live_checker.html')
+
+def about_us(request):
+    return render(request, 'posturepal/about_us.html')
+
+def resources(request):
+    return render(request, 'posturepal/resources.html')
