8467f029da0068b51ffce6ae6429f7159094ce1a
jackawackadoo <jackawackadoo@gmail.com>
2025-03-30 05:05:22 -0400

    Finalized functions and added comments

--- posturepal/static/posturepal/serial_collect.py
+++ posturepal/static/posturepal/serial_collect.py
@@ -3,17 +3,20 @@ import serial.tools.list_ports
 import struct
 
 good_cereal = None
+#Prenegotiated start byte that computer looks for
 start_byte = '@'
 
+cereal = None
 
+# collect X Y and Z data, and initialize serial if it hasn't already
 def update():
+    if cereal == None:
+        cereal = instantiateSerial
     data = ('f', [0.0, 0.0, 0.0])
-    instantiateSerial
-   
-
+    data = collect()
     return data
  
-
+#open and begin serial
 def instantiateSerial():
     ports = list(serial.tools.list_ports.comports())
     for port in ports:
@@ -28,6 +31,21 @@ def instantiateSerial():
             print(f"Error opening serial port: {e}")
     else:
         print("Rf nano not found")
+
+#collect data
+def collect():
+    c = bytes(1)
+    if (cereal.in_waiting>0):
+        c = cereal.read(1)
+        if(c.decode('ascii')=='@'):
+            x = cereal.read(4)
+            fX = struct.unpack('<f', x)[0]
+            y = cereal.read(4)
+            fY = struct.unpack('<f', y)[0]
+            z = cereal.read(4)
+            fZ = struct.unpack('<f', z)[0]
+    data = ('f', [fX, fY, fZ])
+    return data    
 
         
     
